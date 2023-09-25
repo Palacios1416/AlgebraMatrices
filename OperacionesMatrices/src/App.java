c8e40b585e61033aee6a4c1d36063eba86dd4136
Dereck Palacios <dpalaciosurbi@gmail.com>
2023-09-24 22:10:10 -0600

    avanzamos

--- OperacionesMatrices/src/App.java
+++ OperacionesMatrices/src/App.java
@@ -1,35 +1,84 @@
 import java.util.Scanner;
 public class App {
     public static byte opc, sizefilas,sizecolum, sizefilasB,sizecolumB;
-    public static boolean compare;
+    public static byte count = 3; //3 intentos, cada error sera -1
+    public static boolean igualdad;
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("¿Que tipo de operación desea hacer?\n" +
                             "1) Igualdad de matrices\n" +
                             "2) Suma de matrices\n" +
+                            "3) Matriz cero\n" +
+                            "4) Matriz inversa aditiva\n" +
                             "5) Resta de matrices\n" +
+                            "6) Escalar de matriz\n" +
                             "7) Multiplicacion de matrices");
         do {
            opc = scan.nextByte(); 
-        } while (opc < 1 || opc > 3);
+        } while (opc < 1 || opc > 10);
 
         System.out.println("¿Cuantas filas tendra la matriz A?");
                 do {
                     sizefilas = scan.nextByte(); 
-                } while (sizefilas <= 0);
+                    if (sizefilas <= 0) { //condicion de 3-1 intentos
+                        count--; 
+                        System.out.println("El valor debe ser mayor que cero. Intentos restantes: " + count);
+                    }
+                } while (sizefilas <= 0 && count > 0);
+
+                if (count == 0) { //condicion de salida si se acabaron los intentos
+                    System.out.println("Se han agotado los intentos, ejecute de nuevo");
+                    return;
+                }
+
                 System.out.println("¿Cuantas columnas tendra la matriz A?");
                 do {
                     sizecolum = scan.nextByte(); 
-                } while (sizecolum <= 0);
-        System.out.println("¿Cuantas filas tendra la matriz B?");
-                do {
-                    sizefilasB = scan.nextByte(); 
-                } while (sizefilasB <= 0);
-                System.out.println("¿Cuantas columnas tendra la matriz B?");
-                do {
-                    sizecolumB = scan.nextByte(); 
-                } while (sizecolumB <= 0);
+                    if (sizecolum <= 0) {
+                        count--;
+                        System.out.println("El valor debe ser mayor que cero. Intentos restantes: " + count);
+                    }
+                } while (sizecolum <= 0 && count > 0);
+
+                if (count == 0) {
+                    System.out.println("Se han agotado los intentos, ejecute de nuevo");
+                    return;
+                }
+                
+                if (opc == 3 || opc == 4 || opc == 6) {
+                    System.out.println();
+                } else {
+                    System.out.println("¿Cuantas filas tendra la matriz B?");
+                    do {
+                        sizefilasB = scan.nextByte(); 
+                        if (sizefilasB <= 0) {
+                           count--;
+                            System.out.println("El valor debe ser mayor que cero. Intentos restantes: " + count);
+                        }
+                    } while (sizefilasB <= 0 && count > 0);
+
+                    if (count == 0) {
+                        System.out.println("Se han agotado los intentos, ejecute de nuevo");
+                        return;
+                    }
+
+                    System.out.println("¿Cuantas columnas tendra la matriz B?");
+                    do {
+                        sizecolumB = scan.nextByte(); 
+                        if (sizecolumB <= 0) {
+                            count--;
+                            System.out.println("El valor debe ser mayor que cero. Intentos restantes: " + count);
+                        }
+                    } while (sizecolumB <= 0 && count > 0);
+
+                    if (count == 0) {
+                        System.out.println("Se han agotado los intentos, ejecute de nuevo");
+                        return;
+                    }
+                }
+                
+
                 int [][] matrizA = new int[sizefilas][sizecolum];
                 int [][] matrizB = new int[sizefilasB][sizecolumB];
                 int [][] resultado = new int[sizefilas][sizecolum];
@@ -40,9 +89,9 @@ public class App {
             System.out.println("IGUALDAD DE MATRICES");
                 
                 if (matrizA.length != matrizB.length) {
-                    compare = false;
+                    igualdad = false;
                 } else {
-                System.out.println("Ingrese los elementos de la matriz: ");
+                System.out.println("Ingrese los elementos de la matriz A: ");
                 for (int i = 0; i < sizefilas; i++) {
                     for (int j = 0; j < sizecolum; j++) {
                         System.out.print("Matriz A [" + i + "," + j + "] = ");
@@ -50,9 +99,9 @@ public class App {
                     }
                 }
                 
-                System.out.println("Ingrese los elementos de la matriz: ");
-                for (int i = 0; i < sizefilas; i++) {
-                    for (int j = 0; j < sizecolum; j++) {
+                System.out.println("Ingrese los elementos de la matriz B: ");
+                for (int i = 0; i < sizefilasB; i++) {
+                    for (int j = 0; j < sizecolumB; j++) {
                         System.out.print("Matriz B [" + i + "," + j + "] = ");
                         matrizB[i][j] = scan.nextInt();
                     }
@@ -60,19 +109,19 @@ public class App {
                 System.out.println(":::::::::::::::::::::::::::::::::");
 
                     for (int i = 0; i < sizefilas; i++) {
-                    for (int j = 0; j < sizecolum; j++) {
-                        if (matrizA[i][j] != matrizB[i][j]) {
-                            compare = false; 
+                        for (int j = 0; j < sizecolum; j++) {
+                            if (matrizA[i][j] != matrizB[i][j]) {
+                            igualdad = false; 
                             break;     
+                            }
                         }
-                    }
-                    if (!compare) {
+                        if (!igualdad) {
                         break;
-                    }
+                        }
                     }
                 }
                 
-                if (compare) {
+                if (igualdad) {
                     System.out.println("Las matrices son iguales");
                 }else{
                     System.out.println("Las matrices no son iguales");
@@ -85,7 +134,7 @@ public class App {
             System.out.println("SUMA DE MATRICES");
                 
                 if (sizefilas == sizefilasB && sizecolum == sizecolumB) {
-                System.out.println("Ingrese los elementos de la matriz: ");
+                System.out.println("Ingrese los elementos de la matriz A: ");
                 for (int i = 0; i < sizefilas; i++) {
                     for (int j = 0; j < sizecolum; j++) {
                         System.out.print("Matriz A [" + i + "," + j + "] = ");
@@ -93,9 +142,9 @@ public class App {
                     }
                 }
                  
-                System.out.println("Ingrese los elementos de la matriz: ");
-                for (int i = 0; i < sizefilas; i++) {
-                    for (int j = 0; j < sizecolum; j++) {
+                System.out.println("Ingrese los elementos de la matriz B: ");
+                for (int i = 0; i < sizefilasB; i++) {
+                    for (int j = 0; j < sizecolumB; j++) {
                         System.out.print("Matriz B [" + i + "," + j + "] = ");
                         matrizB[i][j] = scan.nextInt();
                     }
@@ -113,14 +162,23 @@ public class App {
                     System.out.println("Para poder sumar las matrices deben tener las mismas dimensiones");
                 }
                 
+                break;
+            case 3:
+            System.out.println(":::::::::::::::::::::::::::::::::");
+            System.out.println("MATRIZ CERO");
+
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolum; j++) {
+                        System.out.print(matrizA[i][j] + "\t");
+                    }
+                    System.out.println();
+                }
 
                 break;
-            case 5:
+            case 4:
+                System.out.println(":::::::::::::::::::::::::::::::::");
+                System.out.println("MATRIZ INVERSA ADITIVA");
 
-            System.out.println(":::::::::::::::::::::::::::::::::");
-            System.out.println("RESTA DE MATRICES");
-              
-                if (sizefilas == sizefilasB && sizecolum == sizecolumB) {
                 System.out.println("Ingrese los elementos de la matriz: ");
                 for (int i = 0; i < sizefilas; i++) {
                     for (int j = 0; j < sizecolum; j++) {
@@ -128,10 +186,33 @@ public class App {
                         matrizA[i][j] = scan.nextInt();
                     }
                 }
-                
-                System.out.println("Ingrese los elementos de la matriz: ");
+                System.out.println(":::::::::::::::::::::::::::::::::");
+                System.out.println("La inversa aditiva es: ");
                 for (int i = 0; i < sizefilas; i++) {
                     for (int j = 0; j < sizecolum; j++) {
+                        System.out.print(matrizA[i][j] * (-1) + "\t");
+                    }
+                    System.out.println();
+                }
+
+            break;
+            case 5:
+
+            System.out.println(":::::::::::::::::::::::::::::::::");
+            System.out.println("RESTA DE MATRICES");
+              
+                if (sizefilas == sizefilasB && sizecolum == sizecolumB) {
+                System.out.println("Ingrese los elementos de la matriz A: ");
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolum; j++) {
+                        System.out.print("Matriz A [" + i + "," + j + "] = ");
+                        matrizA[i][j] = scan.nextInt();
+                    }
+                }
+                
+                System.out.println("Ingrese los elementos de la matriz B: ");
+                for (int i = 0; i < sizefilasB; i++) {
+                    for (int j = 0; j < sizecolumB; j++) {
                         System.out.print("Matriz B [" + i + "," + j + "] = ");
                         matrizB[i][j] = scan.nextInt();
                     }
@@ -150,11 +231,79 @@ public class App {
                 }
 
                 break;
+
+                case 6:
+                
+                System.out.println(":::::::::::::::::::::::::::::::::");
+                System.out.println("ESCALAR POR LA MATRIZ");
+
+                System.out.println("¿Cual es el escalar?");
+                int escalar = scan.nextInt();
+
+                System.out.println("Ingrese los elementos de la matriz: ");
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolum; j++) {
+                        System.out.print("Matriz A [" + i + "," + j + "] = ");
+                        matrizA[i][j] = scan.nextInt();
+                    }
+                }
+
+                System.out.println(":::::::::::::::::::::::::::::::::");
+                System.out.println("El resultado es: ");
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolum; j++) {
+                       
+                        System.out.print(matrizA[i][j] * escalar + "\t");
+                    }
+                    System.out.println();
+                }
+                
+                break;
+
                 case 7:
                 
                 System.out.println(":::::::::::::::::::::::::::::::::");
                 System.out.println("MULTIPLICACION DE MATRICES");
+
+                if (sizecolum != sizefilasB) {
+                    System.out.println("No se pueden multiplicar las matrices");
+                } else {
+                    System.out.println("Ingrese los elementos de la matriz A: ");
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolum; j++) {
+                        System.out.print("Matriz A [" + i + "," + j + "] = ");
+                        matrizA[i][j] = scan.nextInt();
+                    }
+                }
                 
+                System.out.println("Ingrese los elementos de la matriz B: ");
+                for (int i = 0; i < sizefilasB; i++) {
+                    for (int j = 0; j < sizecolumB; j++) {
+                        System.out.print("Matriz B [" + i + "," + j + "] = ");
+                        matrizB[i][j] = scan.nextInt();
+                    }
+                }
+
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolumB; j++) {
+                        for (int k = 0; k < sizecolum; k++) {
+                            resultado[i][j] = matrizA[i][k] * matrizB[i][k];
+                        }
+                    }
+                }
+
+                System.out.println(":::::::::::::::::::::::::::::::::");
+                System.out.println("La multiplicacion de AB es: ");
+
+                for (int i = 0; i < sizefilas; i++) {
+                    for (int j = 0; j < sizecolumB; j++) {
+                        System.out.print(resultado[i][j] + "\t");
+                    }
+                    System.out.println();
+                }
+
+                }
+
                 break;
         }
         
